05a56fe262c8a896828da0473c2eb99c6f3046fa
abhinav-appstechy <abhinav@appstechy.com>
2024-06-11 12:56:38 +0530

    upadated codebase

--- pollingproject/pollingproject/settings.py
+++ pollingproject/pollingproject/settings.py
@@ -138,6 +138,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 MEDIA_URL = "media/"
